14dc838f6e70b48517098d92f68cb0d35d2e261c
Shashank Verma <shashank3959@gmail.com>
2020-03-03 20:04:47 -0600

    Start building the training setup

---
+++ main.py
@@ -0,0 +1,64 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sys import version as python_version
+
+from resnet import ResNet18
+
+def dataloader():
+    # Loading Data
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader, classes
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def display_image(trainloader, classes):
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+if __name__ == "__main__":
+    # Switches
+    display_images = False # Display Random Training Image
+
+
+    print("Python version: ", python_version)
+    print("PyTorch version: ", torch.__version__)
+
+    trainloader, testloader, classes = dataloader()
+
+    if display_images == True:
+        display_image(trainloader, classes)
+
